1b2a12d0e53827828a914ad999c2d999ba8e24bb
Chad Bowen <chadbowen248@gmail.com>
2017-08-02 20:45:08 -0400

    more refactor

--- js/ComicsContainer.jsx
+++ js/ComicsContainer.jsx
@@ -2,6 +2,16 @@ import React from "react";
 // import Axios from "axios"
 import "../public/styles/landing.css";
 
-const ComicsContainer = props => <div>hello</div>;
+const ComicsContainer = props =>
+  <div className="page-container">
+    {Object.keys(props.comics).map(index => {
+      const comic = props.comics[index]
+      return (
+        <p key={index}>
+          {comic.title}
+        </p>
+      );
+    })}
+  </div>;
 
 export default ComicsContainer;
