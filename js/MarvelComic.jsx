432030933021abb1315921a402b1242110e618e4
Chad Bowen <chadbowen248@gmail.com>
2017-08-16 17:24:33 -0400

    sync changes

--- js/MarvelComic.jsx
+++ js/MarvelComic.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const MarvelComic = (props) => 
-<div  className='comic'>
-    <a href={props.href} target='blank'>
-    <img src={props.image.replace('portrait_incredible','detail')} alt={props.title}/>
-    <p className='comic-title'><strong>{props.title}</strong></p>
-  
-    </a>
-</div>
+const MarvelComic = props => {
+    const test = props.image.indexOf('_not_');
+    const normalImage = props.image.replace("portrait_incredible", "detail");
+    console.log(normalImage)
+  return (
+    <div className="comic">
+      <a href={props.href} target="blank">
+        <img
+          src={normalImage}
+          alt={props.title}
+        />
+        <p className="comic-title">
+          <strong>
+            {props.title}
+          </strong>
+        </p>
+      </a>
+    </div>
+  );
+};
 
-
-export default MarvelComic
+export default MarvelComic;
