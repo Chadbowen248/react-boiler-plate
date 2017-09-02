3d8fcc1dd87e48dbd657c381351197351c20e747
Chad Bowen <Chadbowen248@gmail.com>
2017-09-02 15:43:07 -0400

    first steps to add comic to a DB

--- js/ComicCollection.jsx
+++ js/ComicCollection.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React from "react"
+import Axios from "axios"
 
 class ComicCollection extends React.Component {
-    componentWillMount(){}
-    render() {
-        return(
-            <div>hello</div>
-        )
-    }
+
+  searchForComic = () => {
+    const searchTerm = this.textInput.value;
+    const apiKey = '2736f1620710c52159ba0d0aea337c59bd273816'
+    const URL = `https://comicvine.gamespot.com/api/search/?api_key=${apiKey}&format=json&query=${searchTerm}&resources=volume`
+    Axios({url: URL,  crossdomain: true}).then(res => console.log(res.data.results))
+    // fetch(URL, {mode: 'no-cors'}).then(res => console.log(res))
+    this.textInput.value = ""
+
+  }
+  render() {
+    return (
+      <div>
+        <input
+          className="comicSearch"
+          type="text"
+          ref={input => {
+            this.textInput = input
+          }}
+        />
+        <input type="submit" onClick={this.searchForComic} />
+      </div>
+    )
+  }
 }
 
 export default ComicCollection
+
+
+// https://comicvine.gamespot.com/api/search/?api_key=2736f1620710c52159ba0d0aea337c59bd273816&format=json&query=hellboy%20library%20edition&resources=volume
