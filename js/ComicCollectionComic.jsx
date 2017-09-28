5f367954a5d27b9e3b8b78ef2baa76b40693f06b
Chad Bowen <chadbowen248@gmail.com>
2017-09-28 16:53:40 -0400

    more css changes

--- js/ComicCollectionComic.jsx
+++ js/ComicCollectionComic.jsx
@@ -3,11 +3,11 @@ import { shape } from "prop-types"
 
 const ComicCollectionComic = props =>
   <div className="comic-container__comic">
-    <img className="comic-results__image" src={props.details.image.medium_url} alt="" />
+    <img className="comic-collection__image" src={props.details.image.medium_url} alt="" />
     <p className='comic-container__title'>
       {props.details.finalName}
     </p>
-    <p onClick={() => props.removeComic(`comic-${props.details.id}`)}>X</p>
+    <img className="comic-delete" src="public/img/close.png" alt="close" onClick={() => props.removeComic(`comic-${props.details.id}`)}/>
   </div>
 
 export default ComicCollectionComic
